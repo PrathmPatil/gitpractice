4d272a046454151bd1e6501c6072677a2413bbaf
PrathmPatil <prathmpatil2818@gmail.com>
2022-11-10 19:47:52 +0530

    single digit in word

---
+++ sidi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ read -p "Enter One Digit Number:-" num
+
+     case $num in
+         0) echo $num "zero";; 
+         1) echo $num "one";;
+         2) echo $num "two";;
+         3) echo $num "three";;
+         4) echo $num "four";;
+         5) echo $num "five";;
+         6) echo $num "six";;
+         7) echo $num "seven";;
+         8) echo $num "eight";;
+         9) echo $num "nine";;
+    esac
+  
+         
