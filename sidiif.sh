15b9b1af52654f5d1265c825112b5a46eacc3e1e
PrathmPatil <prathmpatil2818@gmail.com>
2022-11-11 13:09:51 +0530

    Single Digit Using if elif

---
+++ sidiif.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+  read -p "Enter Number:-" num
+   
+      if(($num >=0 && $num<=9))
+       then 
+          if(($num == 1))
+            then
+              echo "One";
+          elif(($num ==2))
+            then 
+              echo "Two";
+           elif(($num ==3))
+            then 
+              echo "Three";
+          elif(($num ==4))
+            then 
+              echo "Four";
+          elif(($num ==5))
+            then 
+              echo "Five";
+          elif(($num ==6))
+            then 
+              echo "Six";
+          elif(($num ==7))
+            then 
+              echo "Seven";
+          elif(($num ==8))
+            then 
+              echo "Eight";
+          elif(($num ==9))
+            then 
+              echo "Nine";
+          elif(($num ==0))
+            then 
+              echo "Zero";  
+          fi
+      else
+         echo "number not walid";
+      fi 
